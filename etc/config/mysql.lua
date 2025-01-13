b3a9d5839b7d28547964f369ad3a85e3d736c5a8
cleansky <lijin157@163.com>
2025-01-13 10:07:49 +0800

    get mysql connect param from env

--- etc/config/mysql.lua
+++ etc/config/mysql.lua
@@ -1,12 +1,15 @@
 -- 从环境变量读取数据库密码
+local host = os.getenv("MYSQL_HOST") or "127.0.0.1"
+local user = os.getenv("MYSQL_USER") or "root"
 local password = os.getenv("MYSQL_PASSWORD") or "123456"
+local database = os.getenv("MYSQL_DATABASE") or "tyconn"
 
 return {
     -- 数据库连接配置
     connection = {
-        host = "127.0.0.1",
+        host = host,
         port = 3306,
-        user = "root",
+        user = user,
         password = password,
         charset = "utf8mb4",
         max_packet_size = 1024 * 1024,
@@ -14,7 +17,7 @@ return {
     },
     
     -- 数据库名称
-    database = "tyconn",
+    database = database,
     
     -- 重试配置
     retry = {
