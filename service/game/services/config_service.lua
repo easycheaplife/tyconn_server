48b234caba4d27885ca3880b6cf55193474e780c
sky clean <skyclean@skys-MacBook-Pro.local>
2025-02-25 21:55:44 +0800

    fix read file error

--- service/game/services/config_service.lua
+++ service/game/services/config_service.lua
@@ -113,7 +113,7 @@ end
 
 -- 加载单位配置
 function M.load_unit_config()
-    local data = config_loader.get_config("Dfw_Unit")
+    local data = config_loader.get_config("Dfw_unit")
     if not data then
         return false
     end
