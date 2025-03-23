2e4149fae31a70ba281c7772fb8c49df666c6807
sky clean <skyclean@skys-MacBook-Pro.local>
2025-03-23 11:45:16 +0800

    bug fix log error

--- service/game/services/equip_service.lua
+++ service/game/services/equip_service.lua
@@ -946,7 +946,6 @@ end
 function M.check_equipment_level_upgrades()
     logger.info("Checking equipment level upgrades")
     local count = M.check_equipment_odds_level_upgrades()
-    logger.info("Completed %d equipment level upgrades", count)
 end
 
 return M 
