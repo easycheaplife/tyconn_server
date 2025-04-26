dd7b0c753015142edd57bc4b1515c675dbc243bf
sky clean <skyclean@skys-MacBook-Pro.local>
2025-04-26 11:49:55 +0800

    add random event info for response roll_dice

--- test/lib/handlers/map/roll_dice.js
+++ test/lib/handlers/map/roll_dice.js
@@ -29,6 +29,25 @@ async function rollDice() {
         }));
     }
 
+    // 处理random_events字段
+    if (response && response.random_events) {
+        console.log("随机事件原始数据:");
+        response.random_events.forEach((event, index) => {
+            console.log(`随机事件 ${index}:`, event);
+            console.log(`is_random_event类型: ${typeof event.is_random_event}, 值: ${event.is_random_event}`);
+        });
+
+        // 转换随机事件数据格式
+        response.random_events = response.random_events.map(event => ({
+            event_id: event.event_id,
+            cell_id: event.cell_id,
+            is_random_event: event.is_random_event !== undefined ? event.is_random_event : true // 随机事件默认为true
+        }));
+    } else {
+        // 如果没有随机事件，设置为空数组
+        response.random_events = [];
+    }
+
     return response;
 }
 
